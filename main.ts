c3d1efffd5a5d0baadd3e0bda7f7768762d7d386
adeiaranguren <110352950+adeiaranguren@users.noreply.github.com>
2022-07-31 21:29:22 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Pausuak)
+})
+input.onGesture(Gesture.Shake, function () {
+    Pausuak += Pausuak + 1
+})
+let Pausuak = 0
+Pausuak = 0
